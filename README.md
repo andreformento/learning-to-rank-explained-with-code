bd39f1fabee47f0d46bd9d4ff599a3833000759d
André Formento <andreformento.sc@gmail.com>
2020-05-17 19:24:18 -0300

    configured workspace

--- README.md
+++ README.md
@@ -1,4 +1,9 @@
 # learning-to-rank-explained-with-code
 
+- run with
+```shell
+./run.sh
+```
+
 ## References
 - https://mlexplained.com/2019/05/27/learning-to-rank-explained-with-code/
---
+++ run.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+DIRECTORY="$(pwd)/notebook"
+
+if [[ -d "$DIRECTORY" ]]; then
+    echo "Using cached file"
+else 
+    echo "Downloading files"
+    mkdir notebook
+    cd "$DIRECTORY"
+
+    wget https://s3-us-west-2.amazonaws.com/xgboost-examples/MQ2008.rar
+    unrar x MQ2008.rar
+    mv -f MQ2008/Fold1/*.txt .
+    # convert data format
+    python ../trans_data.py train.txt mq2008.train mq2008.train.group
+    python ../trans_data.py test.txt mq2008.test mq2008.test.group
+    python ../trans_data.py vali.txt mq2008.vali mq2008.vali.group
+
+    cp ../notebook.ipynb .
+
+    cd ..
+fi
+
+docker run -it --rm \
+       --name learning-to-rank-explained-with-code \
+       -v $(pwd)/notebook:/home/jovyan \
+       -p 8888:8888 \
+       jupyter/scipy-notebook
